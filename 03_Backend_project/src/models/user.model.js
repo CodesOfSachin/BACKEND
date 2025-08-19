6c5266804a7cc309a76ac5756d36adabacbca905
CodesOfSachin <codes.of.sachin@gmail.com>
2025-08-19 13:51:47 +0530

    created jwt methods

--- 03_Backend_project/src/models/user.model.js
+++ 03_Backend_project/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
     {
@@ -11,14 +12,14 @@ const userSchema = new Schema(
             trim: true,
             index: true
         },
-        username: {
+        email: {
             type: String,
             required: true,
             unique: true,
             lowercase: true,
             trim: true,
         },
-        username: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -58,6 +59,32 @@ userSchema.pre("save", async function (next) {
 
 userSchema.method.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
 }
 
 export const User = mongoose.model("User", userSchema)
