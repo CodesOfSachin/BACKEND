2aff07ddab0760d9530dccb1e40098806f810b44
CodesOfSachin <codes.of.sachin@gmail.com>
2025-08-30 15:54:55 +0530

    get current user functionality

--- 03_Backend_project/src/controller/user.controller.js
+++ 03_Backend_project/src/controller/user.controller.js
@@ -255,9 +255,17 @@ const changeCurrentPassword = asyncHandler( async( req, res ) => {
     )
 })
 
+const getCurrentUser = asyncHandler( async( req, res ) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+})
+
 export { 
     registerUser, 
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser
 }
